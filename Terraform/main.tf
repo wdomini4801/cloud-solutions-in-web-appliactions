84ff08342db2ff79ef3036bf1230bfae74c9481e
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2024-12-03 01:49:35 +0000

    2 instances (not working yet)

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -52,68 +52,68 @@ resource "aws_route_table_association" "terra_route_table_association" {
   route_table_id = aws_route_table.terra_route_table.id
 }
 
-# Grupy bezpieczeństwa dla instancji app
-resource "aws_security_group" "app_sg" {
+# Grupy bezpieczeństwa dla instancji backend
+resource "aws_security_group" "backend_sg" {
   vpc_id = aws_vpc.terra_vpc.id
-  name = "app-sg"
+  name = "backend-sg"
 
   tags = {
-    Name = "appSG"
+    Name = "backendSG"
   }
 }
 
 # Reguły bezpieczeństwa
-resource "aws_security_group_rule" "allow_app_ssh" {
+resource "aws_security_group_rule" "allow_backend_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.app_sg.id
+  security_group_id = aws_security_group.backend_sg.id
 }
 
-resource "aws_security_group_rule" "allow_app_https" {
+resource "aws_security_group_rule" "allow_backend_https" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.app_sg.id
+  security_group_id = aws_security_group.backend_sg.id
 }
 
-resource "aws_security_group_rule" "allow_app_node" {
+resource "aws_security_group_rule" "allow_backend_node" {
   type              = "ingress"
   from_port         = 3000
   to_port           = 3000
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.app_sg.id
+  security_group_id = aws_security_group.backend_sg.id
 }
 
-resource "aws_security_group_rule" "allow_app_all_outbound" {
+resource "aws_security_group_rule" "allow_backend_all_outbound" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.app_sg.id
+  security_group_id = aws_security_group.backend_sg.id
 }
 
-# Instancja EC2 dla app
-resource "aws_instance" "app_instance" {
+# Instancja EC2 dla backend
+resource "aws_instance" "backend_instance" {
   ami                    = "ami-0866a3c8686eaeeba"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.terra_subnet.id
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
   key_name               = "vockey"
 
   tags = {
-    Name = "app"
+    Name = "backend"
   }
 }
 
 # Tymczasowe połączenie do sprawdzenia, czy maszyna jest dostępna
-resource "terraform_data" "app_ssh_connection" {
+resource "terraform_data" "backend_ssh_connection" {
   provisioner "remote-exec" {
     inline = [
       "echo \"$HOSTNAME connected...\""
@@ -124,36 +124,142 @@ resource "terraform_data" "app_ssh_connection" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("~/.ssh/labsuser2.pem")
-    host        = aws_instance.app_instance.public_ip
+    host        = aws_instance.backend_instance.public_ip
   }
 }
 
 # Konfiguracja pliku inventory dla Ansible
-resource "null_resource" "append_inventory" {
+resource "null_resource" "backend_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-      echo "[app]" >> ./inventory.ini
-      echo "${aws_instance.app_instance.public_ip}" >> ./inventory.ini
+      echo "[backend]" >> ./inventory.ini
+      echo "${aws_instance.backend_instance.public_ip}" >> ./inventory.ini
     EOT
   }
 
   triggers = {
-    instance_ip = aws_instance.app_instance.public_ip
+    instance_ip = aws_instance.backend_instance.public_ip
   }
 }
 
 # Uruchomienie procesu konfiguracji instancji z użyciem Ansible
-resource "terraform_data" "ansible_app_provisioner" {
+resource "terraform_data" "ansible_backend_provisioner" {
   provisioner "local-exec" {
     working_dir = "./"
-    command     = "ansible-playbook playbook.yml"
+    command     = "ansible-playbook playbook_backend.yml"
   }
   depends_on = [
-    terraform_data.app_ssh_connection
+    terraform_data.backend_ssh_connection
   ]
 }
 
 # Wyświetlenie konfiguracji hosta
-output "app_instance_ip_address" {
-  value = aws_instance.app_instance.public_ip
+output "backend_instance_ip_address" {
+  value = aws_instance.backend_instance.public_ip
+}
+
+# Grupy bezpieczeństwa dla instancji frontend
+resource "aws_security_group" "frontend_sg" {
+  vpc_id = aws_vpc.terra_vpc.id
+  name = "frontend-sg"
+
+  tags = {
+    Name = "frontendSG"
+  }
+}
+
+# Reguły bezpieczeństwa
+resource "aws_security_group_rule" "allow_frontend_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "allow_frontend_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "allow_frontend_node" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "allow_frontend_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.frontend_sg.id
+}
+
+# Instancja EC2 dla frontend
+resource "aws_instance" "frontend_instance" {
+  ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.terra_subnet.id
+  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
+  key_name               = "vockey"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+# Tymczasowe połączenie do sprawdzenia, czy maszyna jest dostępna
+resource "terraform_data" "frontend_ssh_connection" {
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"$HOSTNAME connected...\""
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/labsuser2.pem")
+    host        = aws_instance.frontend_instance.public_ip
+  }
+}
+
+# Konfiguracja pliku inventory dla Ansible
+resource "null_resource" "frontend_inventory" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[frontend]" >> ./inventory.ini
+      echo "${aws_instance.frontend_instance.public_ip}" >> ./inventory.ini
+    EOT
+  }
+
+  triggers = {
+    instance_ip = aws_instance.frontend_instance.public_ip
+  }
+}
+
+# Uruchomienie procesu konfiguracji instancji z użyciem Ansible
+resource "terraform_data" "ansible_frontend_provisioner" {
+  provisioner "local-exec" {
+    working_dir = "./"
+    command     = "ansible-playbook playbook_frontend.yml"
+  }
+  depends_on = [
+    terraform_data.frontend_ssh_connection
+  ]
+}
+
+# Wyświetlenie konfiguracji hosta
+output "frontend_instance_ip_address" {
+  value = aws_instance.frontend_instance.public_ip
 }
