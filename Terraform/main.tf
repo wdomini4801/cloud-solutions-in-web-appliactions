35697b460898554886b781173c8201edfa922985
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2024-12-03 00:58:00 +0000

    DEPLOY ON TERRAFORM WORKS!!!!!!!

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -81,6 +81,15 @@ resource "aws_security_group_rule" "allow_app_http" {
   security_group_id = aws_security_group.app_sg.id
 }
 
+resource "aws_security_group_rule" "allow_app_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.app_sg.id
+}
+
 resource "aws_security_group_rule" "allow_app_node" {
   type              = "ingress"
   from_port         = 3000
