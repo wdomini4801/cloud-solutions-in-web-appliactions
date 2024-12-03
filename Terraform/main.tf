d70b8141f851538b8d813c68de9a90c4aa3dc488
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2024-12-03 01:08:15 +0000

    remove unnecessary rule

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -72,15 +72,6 @@ resource "aws_security_group_rule" "allow_app_ssh" {
   security_group_id = aws_security_group.app_sg.id
 }
 
-resource "aws_security_group_rule" "allow_app_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.app_sg.id
-}
-
 resource "aws_security_group_rule" "allow_app_https" {
   type              = "ingress"
   from_port         = 443
