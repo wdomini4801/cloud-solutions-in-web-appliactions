df8086de9aee7079d1dd312e7a01754c8ad55da0
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2024-12-27 14:45:02 +0000

    add proxy in nginx (almost working, without sockets)

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,35 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  
-  const serverIp = `${env.VITE_SERVER_IP}`;
-  const port = `${env.VITE_SERVER_PORT}`;
-  
-  return {
-      server: {
-        proxy: {
-          '/api': {
-            target: `https://${serverIp}:${port}`,
-            ws: true,
-            changeOrigin: true,
-            secure: false,
-            configure: (proxy, _options) => {
-              proxy.on('error', (err, _req, _res) => {
-                console.error('proxy error', err);
-              });
-              proxy.on('proxyReq', (proxyReq, req, _res) => {
-                console.error('Sending Request to the Target:', req.method, req.url);
-              });
-              proxy.on('proxyRes', (proxyRes, req, _res) => {
-                console.error('Received Response from the Target:', proxyRes.statusCode, req.url);
-              });
-            },
-          },
-        },
-      },
-      plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
