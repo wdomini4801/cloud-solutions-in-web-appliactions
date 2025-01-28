73a5d19b8b2bc529b99e823c492c1a118314da89
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-01-28 00:51:09 +0000

    add beanstalk to tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -107,3 +107,128 @@ resource "aws_security_group_rule" "allow_app_all_outbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.app_sg.id
 }
+
+resource "aws_sqs_queue" "websocket_terra_queue" {
+  name                      = "websocket-terra-queue"
+  visibility_timeout_seconds = 40
+  message_retention_seconds  = 345600 # 4 days in seconds
+  delay_seconds              = 0
+  max_message_size           = 262144 # 256 KB
+  receive_wait_time_seconds  = 0
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Id": "__default_policy_ID",
+    "Statement": [
+      {
+        "Sid": "__owner_statement",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "801415982270"
+        },
+        "Action": [
+          "SQS:*"
+        ],
+        "Resource": "arn:aws:sqs:us-east-1:801415982270:"
+      }
+    ]
+  })
+}
+
+output "queue_url" {
+  value = aws_sqs_queue.websocket_terra_queue.id
+}
+
+resource "aws_elastic_beanstalk_application" "tictactoe_terra" {
+  name        = "tictactoe-terra"
+}
+
+resource "aws_elastic_beanstalk_application_version" "tictactoe_version" {
+  name        = "terra"
+  application = "tictactoe-terra"
+  bucket      = "wdomini4801-bucket"
+  key         = "archive.zip"
+}
+
+resource "aws_elastic_beanstalk_environment" "terra_env" {
+  name                = "Terra-env"
+  application         = aws_elastic_beanstalk_application.tictactoe_terra.name
+  version_label       = aws_elastic_beanstalk_application_version.tictactoe_version.name
+  solution_stack_name = "64bit Amazon Linux 2 v4.0.6 running Docker"
+  tier    = "WebServer"
+  setting {
+      namespace = "aws:autoscaling:asg"
+      name = "MinSize"
+      value = "1"
+  }
+  setting {
+      namespace = "aws:autoscaling:asg"
+      name = "MaxSize"
+      value = "1"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:environment"
+      name = "EnvironmentType"
+      value = "SingleInstance"
+  }
+  setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "EC2KeyName"
+      value = "vockey"
+  }
+  setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "IamInstanceProfile"
+      value = "LabInstanceProfile"
+  }
+  setting {
+      namespace = "aws:ec2:vpc"
+      name = "VPCId"
+      value = aws_vpc.terra_vpc.id
+  }
+  setting {
+      namespace = "aws:ec2:vpc"
+      name = "Subnets"
+      value = aws_subnet.terra_subnet.id
+  }
+  setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "SecurityGroups"
+      value = aws_security_group.app_sg.id
+  }
+  setting {
+      namespace = "aws:ec2:vpc"
+      name = "AssociatePublicIpAddress"
+      value = "true"
+  }
+  setting {
+      namespace = "aws:ec2:instances"
+      name = "InstanceTypes"
+      value = "t3.micro"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:environment"
+      name = "ServiceRole"
+      value = "LabRole"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:command"
+      name = "DeploymentPolicy"
+      value = "AllAtOnce"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:command"
+      name = "BatchSizeType"
+      value = "Percentage"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:command"
+      name = "BatchSize"
+      value = "100"
+  }
+  setting {
+      namespace = "aws:elasticbeanstalk:command"
+      name = "IgnoreHealthCheck"
+      value = "false"
+  }
+}
